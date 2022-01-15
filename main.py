04eed2d2331a831b58e342de230a4e4848cb984f
wolfpack32 <rmdtilgner@gmail.com>
2022-01-15 01:48:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,21 @@
 """
 Created on Fri Jan 14 20:20:03 2022
 
-@author: rmdti
+@author: Ryan Tilgner
+        Reece Tilgner
 """
 
-import PyPDF2
+import PyPDF2 as PyDF
 
-test_pdf = open('JuneDoc.pdf', mode='rb')
-pdfdoc = PyPDF2.PdfFileReader(test_pdf)
+### Opens a file, important to use mode='rb' for binary read mode
+### 
+in_file = open("JuneDoc.pdf", mode="rb")
 
-print(pdfdoc.documentInfo)
+### Converts the in_file to a PyPDF2 object
+doc = PyDF.PdfFileReader(in_file)
+
+### Creates a list of individual pages
+pages = []
+for i in range(doc.getNumPages()):
+    pages.append(doc.getPage(i))
+    
